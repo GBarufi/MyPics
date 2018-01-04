28927aaed45a7b5b9785755aa99ca9418ce02e94
Guilherme de Melo <gm.barufi@gmail.com>
2018-01-04 17:43:52 -0200

    Initial project

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,23 @@
+angular.module('mypics').controller('FotosController', function($scope, recursoFoto) {
+	
+	$scope.fotos = [];
+	$scope.filtro = '';
+	$scope.mensagem = '';
+
+	recursoFoto.query(function(fotos) {
+		$scope.fotos = fotos;
+	}, function(erro) {
+		console.log(erro);
+	});
+
+	$scope.remover = function(foto) {
+		recursoFoto.delete({ fotoId: foto._id}, function(retorno) {
+			var idx = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(idx, 1);
+			$scope.mensagem = "Foto " + foto.titulo + " removida com sucesso.";
+		}, function(erro) {
+			console.log(erro);
+			$scope.mensagem = "Não foi possível remover a foto " + foto.titulo;
+		});
+	};
+});
